0fc723fbbc1145098db3616ba43c4994245491b8
Joy <stepjoyous@gmail.com>
2019-04-03 01:49:45 -0700

    Created Post model, User controllers, and seeded db data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-# Invoke bcrypt to require AND hash passwords
-has_secure_password
-#If User is deleted, its posts are deleted as well
-
-#Checks if email already exists in database
+    # Invoke bcrypt to require AND hash passwords
+    has_secure_password
+    #If User is deleted, its posts are deleted as well
+    has_many :posts, dependent: :destroy
+    #Checks if email already exists in database
+    validates_uniqueness_of :email
 end
